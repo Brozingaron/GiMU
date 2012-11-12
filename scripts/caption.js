0a2c5c58044a6cf6863ae7ac9922f47566c775dc
Andrew B <datadude3@gmail.com>
2012-11-11 21:08:43 -0500

    Renamed to Brozingaron
    - Changed the site's title to match the new development team name
    - New blog post
    - Brozingaron now proudly celebrates Lemon Tuesday

--- scripts/caption.js
+++ scripts/caption.js
@@ -1,2 +1,13 @@
-var captionArray = ['Use ALL the references!','Not unlike Skyrim','Open Source!','Chandeliers!','Contains 0 arrows, 0 knees, and 8 arrow to the knee references','Squids!','Javascript!','Impossible Geometry!','Physics!','Science!','Hi!','Recursion!','Yours for only free-nintey-nine!','Visit our blag!','Long release cycles!','3-Dimmensional!','Not anti-aliased!','Cross-Platform!'];
+var captionArray = ['Use ALL the references!','Not unlike Skyrim','Open Source!','Chandeliers!','Contains 0 arrows, 0 knees, and 8 arrow to the knee references','Squids!','Javascript!','Impossible Geometry!','Physics!','Science!','Hi!','Recursion!','Yours for only free-nintey-nine!','Visit our blag!','Long release cycles!','3-Dimmensional!','Not anti-aliased!','Cross-Platform!','It&#146;s the best of games!'];
 document.getElementById('caption').innerHTML = captionArray[Math.floor(Math.random()*captionArray.length)];
+var today = new Date();
+var y = today.getFullYear();
+if ( y == 2012 ){
+	var m = today.getMonth()+1;
+	if ( m == 11 ){
+		var d = today.getDate();
+		if ( d == 20 ){
+			document.getElementById('caption').innerHTML = "Happy Lemon Tuesday!";
+		};
+	};
+};
