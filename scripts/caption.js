7a9f3824c5d80dbaeecee7eea7e1827c0bbc48e4
Andrew B <datadude3@gmail.com>
2012-11-17 19:34:32 -0500

    Sharing & Stuff
    - Added sharing buttons to the homepage
    - Added more captions to the homepage banner
    - New blog post

--- scripts/caption.js
+++ scripts/caption.js
@@ -1,5 +1,6 @@
-var captionArray = ['Use ALL the references!','Not unlike Skyrim','Open Source!','Chandeliers!','Contains 0 arrows, 0 knees, and 8 arrow to the knee references','Squids!','Javascript!','C#!','Impossible Geometry!','Physics!','Science!','Hi!','Recursion!','Yours for only free-nintey-nine!','Visit our blag!','Long release cycles!','3-Dimmensional!','Not anti-aliased!','Cross-Platform!','It&#146;s the best of games!','Filler images!'];
-document.getElementById('caption').innerHTML = captionArray[Math.floor(Math.random()*captionArray.length)];
+var captionArray = ['Use ALL the references!','Not unlike Skyrim','Open Source!','Chandeliers!','Contains 0 arrows, 0 knees, and 8 arrow to the knee references','Squids!','Javascript!','C#!','Impossible Geometry!','Physics!','Science!','Recursion!','Yours for only free-ninety-nine!','Visit our blag!','Long release cycles!','3-Dimensional!','Not anti-aliased!','Cross-Platform!','It&#146;s the best of games!','Filler images!','2-dimensional planets','1-dimensional time!','SMOOOOOOOOOOOSH!','Uses Google+!','Book our Face!','Share us to your birds!','Tumbl over us!','Random text!','It&#146;s a game!','31 levels of randomness!'];
+var random = Math.floor(Math.random()*captionArray.length)
+document.getElementById('caption').innerHTML = captionArray[random];
 var today = new Date();
 var y = today.getFullYear();
 if ( y == 2012 ){
